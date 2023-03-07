d6bc53bf8e474f1f38a26408d6f96c18b66020a2
Julia Novikova <julikova.n@gmail.com>
2023-03-07 20:52:34 +0300

    Update TESTS

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -66,125 +66,13 @@ public class BonusServiceTest {
     }
 
     @org.junit.jupiter.api.Test
-    void shouldCalculateForRegisteredAndUnderLimitNegative() {
-        BonusService service = new BonusService();
-
-        // подготавливаем данные:
-        long amount = 2_000;
-        boolean registered = true;
-        long expected = 500;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        assertEquals(expected, actual);
-    }
-
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForNotRegisteredAndUnderLimitNegativeNegative() {
-        BonusService service = new BonusService();
-
-        // подготавливаем данные:
-        long amount = 1_000;
-        boolean registered = false;
-        long expected = 30;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        assertEquals(expected, actual);
-    }
-
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForRegisteredAndOverLimitNegative() {
-        BonusService service = new BonusService();
-
-        // подготавливаем данные:
-        long amount = 2_000_000;
-        boolean registered = true;
-        long expected = 400;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        assertEquals(expected, actual);
-    }
-
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForBorderBonus501Negative() {
+    void shouldCalculateForBorderBonus501() {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
         long amount = 50_100;
         boolean registered = false;
-        long expected = 501;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        assertEquals(expected, actual);
-    }
-
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForOverBorderNegative() {
-        BonusService service = new BonusService();
-
-        // подготавливаем данные:
-        long amount = 180_000;
-        boolean registered = true;
-        long expected = 600;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        assertEquals(expected, actual);
-    }
-
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForBorderBonus0() {
-        BonusService service = new BonusService();
-
-        // подготавливаем данные:
-        long amount = 0;
-        boolean registered = true;
-        long expected = 0;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        assertEquals(expected, actual);
-    }
-
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForBorderBonusMinus1Negative() {
-        BonusService service = new BonusService();
-
-        // подготавливаем данные:
-        long amount = -100;
-        boolean registered = false;
-        long expected = -1;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        assertEquals(expected, actual);
-    }
-
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForNegativeBonusNegative() {
-        BonusService service = new BonusService();
-
-        // подготавливаем данные:
-        long amount = -10_000;
-        boolean registered = false;
-        long expected = -100;
+        long expected = 500;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
